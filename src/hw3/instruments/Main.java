b98699565ceff33365aabeb9874212e440b2ac13
vmakar9 <zmerin212@gmail.com>
2022-11-23 19:16:57 +0200

    1

--- src/hw3/instruments/Main.java
+++ src/hw3/instruments/Main.java
@@ -1,12 +1,19 @@
 package hw3.instruments;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Instrument[] instruments = {new Guitar(8),new Drum(16),new Trumpet(12)};
 
-        for (int i = 0; i <instruments.length ; i++) {
-             instruments[i].play();
+        List<Instrument> instrumentList = new ArrayList<>();
+        instrumentList.add(new Guitar(8));
+        instrumentList.add(new Drum(15));
+        instrumentList.add(new Trumpet(12));
+
+
+        for(Instrument instrument: instrumentList){
+            instrument.play();
         }
-
     }
 }
--- src/hw3/reading/Main.java
+++ src/hw3/reading/Main.java
@@ -1,7 +1,12 @@
 package hw3.reading;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-     Printable[] printables = {new Book(),new Magazine()};
+     List<Printable> printableList = new ArrayList<>();
+      printableList.add(new Magazine());
+      printableList.add(new Book());
     }
 }
